ccff97e856162078b7891f2a7b85405db6f261f6
Hudson Afonso <hudson.afonso@gmail.com>
2023-04-19 12:25:20 -0300

    version: 1.0.26+1026

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "version.json": "815b41d2dc40701e744b75b61a7288d5",
+  "version.json": "5f53f7ecd1bd87caeb395a2f91a42056",
 "favicon.ico": "8d4ce4564529e7222760de5454aeed97",
-"index.html": "e5d3361c93ca44fc8d58123ddd38ffa3",
-"/": "e5d3361c93ca44fc8d58123ddd38ffa3",
-"main.dart.js": "22e8c7973da6be9af045801149100897",
+"index.html": "c772882ae46490f275dc8fd4420acf3a",
+"/": "c772882ae46490f275dc8fd4420acf3a",
+"main.dart.js": "bfc606bf6c01e5cc29e3c66a78a2063e",
 "flutter.js": "a85fcf6324d3c4d3ae3be1ae4931e9c5",
 "icons/apple-touch-icon.png": "5defce1495eb4ed5f68d9c92e01db744",
 "icons/icon-192.png": "fd312d96fd8d1f7200f435d7c142c265",
@@ -15,7 +15,7 @@ const RESOURCES = {
 "icons/icon-512-maskable.png": "5ada75e6df463c9465470d085818ea29",
 "icons/icon-512.png": "e7d7402bcaf5a088f46883500cd5d307",
 "manifest.json": "7b3a5e070d8031ca259b1c83748ef449",
-"main.dart.js_1.part.js": "72aaabc8bd27fe0fdbabc8fb2e156e8a",
+"main.dart.js_1.part.js": "55315f127a7b50da099f6163c49fd2f1",
 "assets/AssetManifest.json": "b0ededcf9cd176a9e29ca37ac011a55a",
 "assets/NOTICES": "6dd6867779336f13175817650c70f8ca",
 "assets/FontManifest.json": "cf3c681641169319e61b61bd0277378f",
